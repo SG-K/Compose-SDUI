6ede1f64d0116108fbee9e5222a52e2069a37f53
sg_kantamani <sg.kantamani@gmail.com>
2023-08-22 12:40:28 +0800

    added column test

--- app/src/main/java/com/sgk/foodapphomepagesdui/ui/widgets/ColumnRenderer.kt
+++ app/src/main/java/com/sgk/foodapphomepagesdui/ui/widgets/ColumnRenderer.kt
@@ -3,8 +3,11 @@ package com.sgk.foodapphomepagesdui.ui.widgets
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.sgk.sduicore.modal.Text as TextElement
 import com.sgk.sduicore.modal.metadata.ElementStyle
 import com.sgk.sduicore.modal.metadata.Length
+import com.sgk.sduicore.modal.metadata.Padding
+import com.sgk.sduicore.modal.metadata.TextStyle
 import com.sgk.sduicore.modal.Column as ColumnElement
 
 @Composable
@@ -20,19 +23,43 @@ fun ColumnRenderer(element: ColumnElement) {
 
 @Preview(name = "Column", "Layout Components")
 @Composable
-fun Column() {
+fun ColumnPreview() {
 
-ColumnRenderer(
-  element =ColumnElement(
-    children = emptyList(),
-    style = ElementStyle(
-      width = Length.Max,
-      height = Length.Number(60),
-      background = "Red",
-      id = "root"
+  ColumnRenderer(
+    element = ColumnElement(
+      children = listOf(
+        TextElement(
+          text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          textStyle = TextStyle(
+            textSize = 24,
+            isBold = true,
+          ), style = ElementStyle(
+            padding = Padding(
+              top = 12,
+              bottom = 12,
+              left = 12,
+              right = 12
+            )
+          )
+        ),
+        TextElement(
+          text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper sodales erat vel egestas. In nec diam non est volutpat convallis et ut urna. Aliquam ante libero, sollicitudin dictum magna sit amet, pharetra semper justo. Sed gravida, odio vitae iaculis dignissim, turpis metus faucibus nisi, non aliquam tellus erat et tortor. Duis egestas metus in nisi scelerisque, eu gravida lectus iaculis. Morbi eu nisl dolor. Nullam vel turpis porttitor tellus consequat lobortis. Cras lobortis lectus vel turpis feugiat, ut euismod odio venenatis.",
+          textStyle = TextStyle(
+            textSize = 14,
+            isBold = false,
+          ),
+          style = ElementStyle(
+            padding = Padding(
+              top = 12,
+              bottom = 12,
+              left = 12,
+              right = 12
+            )
+          )
+        )
+      )
     )
   )
-)
 
 
 //  ColumnRenderer(
