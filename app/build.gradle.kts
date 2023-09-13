821c55b0db87485840ef41cbe04310312c827573
sg_kantamani <sg.kantamani@gmail.com>
2023-09-13 20:26:46 +0530

    updated test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,19 +104,12 @@ dependencies {
     implementation ("com.squareup.moshi:moshi-kotlin:1.14.0")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    testImplementation("junit:junit:4.12")
-    testImplementation("junit:junit:4.12")
-
 
 
     // Coroutine
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 
     // Hilt for DI
     implementation ("com.google.dagger:hilt-android:2.44")
@@ -132,37 +125,21 @@ dependencies {
     implementation(project(":features:profile"))
 
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-//    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Local unit tests
     testImplementation ("androidx.test:core:1.4.0")
     testImplementation ("junit:junit:4.13.2")
     testImplementation ("androidx.arch.core:core-testing:2.1.0")
-//    testImplementation "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1"
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation ("com.google.truth:truth:1.1.3")
     testImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
     testImplementation ("io.mockk:mockk:1.10.5")
-//    debugImplementation "androidx.compose.ui:ui-test-manifest:1.1.0-alpha04"
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-
-    // Optional -- Robolectric environment
-//    testImplementation "androidx.test:core:$androidXTestVersion"
-//    // Optional -- Mockito framework
-//    testImplementation "org.mockito:mockito-core:$mockitoVersion"
-//    // Optional -- mockito-kotlin
-//    testImplementation "org.mockito.kotlin:mockito-kotlin:$mockitoKotlinVersion"
-//    // Optional -- Mockk framework
-//    testImplementation "io.mockk:mockk:$mockkVersion"
-
-
+    // Android UI tests
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
     androidTestImplementation ("com.google.dagger:hilt-android-testing:2.37")
     kaptAndroidTest ("com.google.dagger:hilt-android-compiler:2.37")
     androidTestImplementation ("junit:junit:4.13.2")
@@ -172,10 +149,7 @@ dependencies {
     androidTestImplementation ("androidx.test.ext:junit:1.1.3")
     androidTestImplementation ("androidx.test:core-ktx:1.4.0")
     androidTestImplementation ("com.squareup.okhttp3:mockwebserver:4.9.1")
-//    androidTestImplementation "io.mockk:mockk-android:1.10.5"
     androidTestImplementation ("androidx.test:runner:1.4.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    debugImplementation("androidx.compose.ui:ui-test-junit4")
-//    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
 
 }
